41f6fc007e4192bd9b817d5bbf92aeacd9afa0d7
Sergey Ukolov <zezic51@yandex.ru>
2020-08-19 23:21:54 +0300

    Did I disabled the sourcemaps?

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,45 @@
 const path = require('path');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: "./dist/",
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader"
-      }
-    }]
-  },
-  devtool: 'eval',
-  devServer: {
-    publicPath: "/",
-    contentBase: "./dist",
-    hot: true,
-    watchContentBase: true
-  },
-  plugins: [
-    // new BundleAnalyzerPlugin({
-    //   analyzerPort: 9000
-    // })
-  ]
+module.exports = env => {
+
+  let devtool = 'eval';
+  let mode = 'development';
+
+  if (env.NODE_ENV === 'prod') {
+    devtool = 'hidden-source-map';
+    mode = 'production';
+  }
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      publicPath: "./dist/",
+      filename: 'bundle.js'
+    },
+    mode,
+    module: {
+      rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }]
+    },
+    devtool,
+    devServer: {
+      publicPath: "/",
+      contentBase: "./dist",
+      hot: true,
+      watchContentBase: true
+    },
+    plugins: [
+      new BundleAnalyzerPlugin({
+        analyzerPort: 9000
+      })
+    ]
+  }
+
 }
